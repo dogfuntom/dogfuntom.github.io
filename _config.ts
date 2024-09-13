d97cbfd6db5d9fe0af38f8e92e1072ea7d2a6c29
Maxim Kamalov <905138+dogfuntom@users.noreply.github.com>
2024-09-14 01:49:42 +0300

    ➕ Multilanguage: used this plugin
    Automatically changing `lang` based on suffix convention.

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from 'lume/mod.ts'
 import abbr from 'npm:markdown-it-abbr'
+import multilanguage from 'lume/plugins/multilanguage.ts'
 
 // Set the markdown plugins
 const markdown = {
@@ -8,11 +9,37 @@ const markdown = {
 
 const site = lume({}, { markdown })
 
+handleLanguages()
+
 // Copy the "img" directory to _site/images
 site.copy('img', 'images')
 // Copy the content of "assets" directory to the root
 site.copy('assets', '.')
 
 site.ignore('docs')
+
+function handleLanguages() {
+  // "Convert" lang suffix into lang in data.
+  site.parseBasename((basename) => {
+    const match = basename.match(/(.+)_ru/)
+
+    if (match) {
+      const [, basename] = match
+
+      return {
+        lang: 'ru',
+        basename,
+      }
+    }
+  })
+
+  // Use the readied lang data.
+  site.use(
+    multilanguage({
+      languages: ['en', 'ru'], // Available languages
+      defaultLanguage: 'en', // The default language
+    })
+  )
+}
 
 export default site
