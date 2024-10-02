56114a51e7dbf6694113512a897ab6fcb4b6c78d
Maxim Kamalov <905138+dogfuntom@users.noreply.github.com>
2024-10-03 01:03:21 +0300

    🎨 auto-formatted everything

--- _config.ts
+++ _config.ts
@@ -1,15 +1,15 @@
-import lume from "lume/mod.ts";
-import abbr from "npm:markdown-it-abbr";
+import lume from 'lume/mod.ts'
+import abbr from 'npm:markdown-it-abbr'
 
 // Set the markdown plugins
 const markdown = {
   plugins: [abbr],
-};
+}
 
-const site = lume({}, { markdown });
+const site = lume({}, { markdown })
 
-site.copy("assets", ".");
+site.copy('assets', '.')
 
-site.ignore("README.md");
+site.ignore('README.md')
 
-export default site;
+export default site
